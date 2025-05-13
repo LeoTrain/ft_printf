18114140851b83917e9d1b9660c44f4703f97dc3
LeoAsahi <leonardo.bertonasco01@gmail.com>
2025-05-13 10:33:28 +0200

    Copied: from school repo, passed moulinette

---
+++ ft_printf/srcs/arg_to_int.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   arg_to_int.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leberton <leberton@42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 23:50:40 by leberton          #+#    #+#             */
+/*   Updated: 2025/05/08 23:52:34 by leberton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/ft_printf.h"
+
+int	arg_to_int(int n, int plus)
+{
+	size_t		len;
+	char		*num;
+
+	num = ft_itoa(n);
+	len = ft_strlen(num);
+	if (plus && n >= 0)
+		len += write(1, "+", 1);
+	write(1, num, ft_strlen(num));
+	free(num);
+	return (len);
+}
---
+++ ft_printf/srcs/ft_printf.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: leberton <leberton@42vienna.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/09 00:04:53 by leberton          #+#    #+#             */
+/*   Updated: 2025/05/09 00:29:32 by leberton         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../headers/ft_printf.h"
+
+static int	putchar(int c)
+{
+	char	a;
+
+	a = (char)c;
+	return (write(1, &a, 1));
+}
+
+static int	execute_format(va_list *args, const char *c)
+{
+	if (*c == '%')
+		return (write(1, "%", 1));
+	else if (*c == 'c')
+		return (putchar(va_arg(*args, int)));
+	else if (*c == 's')
+		return (arg_to_str(va_arg(*args, char *)));
+	else if (*c == 'p')
+		return (arg_to_ptr(va_arg(*args, unsigned long long)));
+	else if (*c == 'd' || *c == 'i')
+		return (arg_to_int(va_arg(*args, int), 0));
+	else if (*c == 'u')
+		return (arg_to_uint(va_arg(*args, unsigned int)));
+	else if (*c == 'x' || *c == 'X')
+		return (arg_to_hexa(va_arg(*args, int), *c == 'X'));
+	return (0);
+}
+
+int	ft_printf(const char *s, ...)
+{
+	va_list	args;
+	int		len;
+
+	len = 0;
+	va_start(args, s);
+	while (*s)
+	{
+		if (*s == '%')
+			len += execute_format(&args, ++s);
+		else
+			len += putchar(*s);
+		s++;
+	}
+	va_end(args);
+	return (len);
+}
